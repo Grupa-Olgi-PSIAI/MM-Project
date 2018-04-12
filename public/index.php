1ff26779bc13780d0a3ced05d94b4cd1686deef9
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2018-04-13 00:59:20 +0200

    Add MVC (#1)
    * Add MVC with error handling
    
    * Add redirect from main folder
    
    * Fix auto loading

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+use core\Router;
+
+spl_autoload_register(function ($class_name) {
+    $class_name = str_replace('\\', '/', $class_name);
+    require(dirname(__DIR__) . '/' . $class_name . '.php');
+});
+
+error_reporting(E_ALL);
+set_error_handler('core\ErrorHandler::handleError');
+set_exception_handler('core\ErrorHandler::handleException');
+
+$router = new Router();
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+$router->dispatch($_SERVER['QUERY_STRING']);

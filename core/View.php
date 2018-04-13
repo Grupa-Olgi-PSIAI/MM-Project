bbeb632de845955e7b167ffc8056242d819cf6a7
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2018-04-13 08:47:47 +0200

    Add method rendering view without menus

--- core/View.php
+++ core/View.php
@@ -6,7 +6,7 @@ namespace core;
 abstract class View
 {
     /**
-     * Render a view file
+     * Render a view file with menus
      *
      * @param string $view The view file
      * @param array $args Associative array of data to display in the view (optional)
@@ -19,15 +19,53 @@ abstract class View
         extract($args, EXTR_SKIP);
         $file = dirname(__DIR__) . "/view/$view";
         if (is_readable($file)) {
-            self::renderMenu();
-            require $file;
+            self::renderHTML($file);
         } else {
             throw new \Exception("$file not found");
         }
     }
 
+    /**
+     * Render a view without menu
+     *
+     * @param string $view The view file
+     * @param array $args Associative array of data to display in the view (optional)
+     *
+     * @throws \Exception
+     */
+    public static function renderWithoutMenu($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+        $file = dirname(__DIR__) . "/view/$view";
+        if (is_readable($file)) {
+            self::renderHTML($file, false);
+        } else {
+            throw new \Exception("$file not found");
+        }
+    }
+
+    private static function renderHTML($file, $showMenu = true)
+    {
+        self::renderHeader();
+        if ($showMenu) {
+            self::renderMenu();
+        }
+        require $file;
+        self::renderFooter();
+    }
+
     private static function renderMenu()
     {
         require dirname(__DIR__) . "/view/default.html";
+    }
+
+    private static function renderHeader()
+    {
+        require dirname(__DIR__) . "/view/header.html";
+    }
+
+    private static function renderFooter()
+    {
+        require dirname(__DIR__) . "/view/footer.html";
     }
 }

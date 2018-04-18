8582c69e82c9f49b9968da019debf3336c8972e7
Olga Chróstowska <ochrostowska@gmail.com>
2018-04-18 23:53:18 +0200

    apply style for invoices table

--- view/invoicesList.php
+++ view/invoicesList.php
@@ -6,42 +6,47 @@ use util\DateUtils;
 
 <div id="page">
 
-    <a href="/invoices/add" class="btn btn-primary" style="font-size: larger; background-color: #FFC400; color: white; padding: 5px;">Dodaj nową fakturę</a>
+    <a href="/invoices/add" class="btn btn-primary"
+       style="font-size: larger; background-color: #FFC400; color: white; padding: 5px;">Dodaj nową fakturę</a>
     <br>
     <h2>Lista faktur</h2>
-    <table class="table">
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>ID</th>
-            <th>Numer</th>
-            <th>Data</th>
-            <th>Kwota netto</th>
-            <th>Kwota brutto</th>
-            <th>Podatek</th>
-            <th>Waluta</th>
-            <th>Kwota netto w walucie</th>
-            <th>Kontrahent</th>
-        </tr>
-        </thead>
+    <div class="tbl-header">
+        <table cellpadding="0" cellspacing="0" border="0">
+            <thead>
+            <tr>
+                <th>Numer</th>
+                <th>Data</th>
+                <th>Kwota netto</th>
+                <th>Kwota brutto</th>
+                <th>Podatek</th>
+                <th>Waluta</th>
+                <th>Kwota netto w walucie</th>
+                <th>Kontrahent</th>
+            </tr>
+            </thead>
+        </table>
+    </div>
+    <div class="tbl-content">
+        <table cellpadding="0" cellspacing="0" border="0">
+            <tbody>
+            <?php if (isset($invoices)) {
+            foreach ($invoices
 
-        <tbody>
-        <?php if (isset($invoices)) {
-            foreach ($invoices as $key => $invoice) { ?>
-                <tr>
-                    <th scope="row"><?php echo $key ?></th>
-                    <td><?php echo $invoice->getId() ?></td>
-                    <td><?php echo $invoice->getNumber() ?></td>
-                    <td><?php DateUtils::getPlainDate($invoice->getInvoiceDate()) ?></td>
-                    <td><?php echo $invoice->getAmountNet() ?></td>
-                    <td><?php echo $invoice->getAmountGross() ?></td>
-                    <td><?php echo $invoice->getAmountTax() ?></td>
-                    <td><?php echo $invoice->getCurrency() ?></td>
-                    <td><?php echo $invoice->getAmountNetCurrency() ?></td>
-                    <td><?php echo $invoice->getContractorId() ?></td>
-                </tr>
+            as $key => $invoice) { ?>
+            <tr>
+                <td><?php echo $invoice->getNumber() ?></td>
+                <td><?php DateUtils::getPlainDate($invoice->getInvoiceDate()) ?></td>
+                <td><?php echo $invoice->getAmountNet() ?></td>
+                <td><?php echo $invoice->getAmountGross() ?></td>
+                <td><?php echo $invoice->getAmountTax() ?></td>
+                <td><?php echo $invoice->getCurrency() ?></td>
+                <td><?php echo $invoice->getAmountNetCurrency() ?></td>
+                <td><?php echo $invoice->getContractorId() ?></td>
+            </tr>
             <?php }
-        } ?>
-        </tbody>
-    </table>
+            } ?>
+            </tbody>
+        </table>
+    </div>
+
 </div>

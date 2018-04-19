8957821fa7124da79bcd9ec74597d1177ee9392d
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2018-04-19 23:28:38 +0200

    Add Resource model

---
+++ model/Resource.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace model;
+
+
+use core\Model;
+
+class Resource extends Model
+{
+    private $id;
+    private $name;
+
+    /**
+     * Get fields for database
+     * @return array with all class fields and their values
+     */
+    public function getFields(): array
+    {
+        return get_object_vars($this);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+}

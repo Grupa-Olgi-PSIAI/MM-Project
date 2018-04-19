25472e7ac5066ba35278d8d1d86a5a07b630eec1
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2018-04-20 00:00:41 +0200

    Add permission repository stub

---
+++ repository/PermissionRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace repository;
+
+
+use core\Repository;
+use model\Permission;
+
+class PermissionRepository extends Repository
+{
+
+    /**
+     * PermissionRepository constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct("permissions", Permission::class);
+    }
+}

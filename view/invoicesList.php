b585a36e9253d5705c1b715b5205376ae7ff93dd
roleksy <herillus2@gmail.com>
2018-04-23 16:58:07 +0200

    list and filtering buttons

--- view/invoicesList.php
+++ view/invoicesList.php
@@ -21,6 +21,17 @@ use util\DateUtils;
 
     </form>
 
+    <div>
+        <label>Od:</label>
+        <input type='date' name='invoice_date' />
+
+        <label>Do:</label>
+        <input type='date' name='invoice_date' />
+
+        <a href="/invoices/show" class="btn btn-primary"
+           style="font-size: larger; background-color: #FFC400; color: white; padding: 5px;">Filtruj</a>
+    </div>
+
     <br>
     <h2>Lista faktur</h2>
     <div class="tbl-header">
@@ -57,27 +68,11 @@ use util\DateUtils;
                         <td><?php echo $invoice->getCurrency() ?></td>
                         <td><?php echo $invoice->getAmountNetCurrency() ?></td>
                         <td><?php echo $invoice->getContractorId() ?></td>
-                        <td><?php echo '<a href="/invoices/details?id=' . $invoice->getId() . '" class="btn btn-primary">Szczegóły</a>'; ?></td>
-                        <td><?php echo '<a href="/invoices/edit?id=' . $invoice->getId() . '" class="btn btn-primary">Edytuj</a>'; ?></td>
-                        <td><?php echo '<a href="/invoices/delete?id=' . $invoice->getId() . '">Usuń</a>' ?></td>
+                        <td><?php echo '<a href="/invoices/details?id=' . $invoice->getId() . '" class="btn btn-primary"><button>Szczegóły</button></a>'; ?></td>
+                        <td><?php echo '<a href="/invoices/edit?id=' . $invoice->getId() . '" class="btn btn-primary"><button>Edytuj</button></a>'; ?></td>
+                        <td><?php echo '<a href="/invoices/delete?id=' . $invoice->getId() . '"><button>Usuń</button></a>' ?></td>
                     </tr>
                 <?php }
-            } ?>
-            <?php if (isset($invoice)) { ?>
-                    <tr>
-                        <td><?php if(isset($invoice)){ echo $invoice->getNumber() ;}?>/td>
-                        <td><?php if(isset($invoice)){ DateUtils::getPlainDate($invoice->getInvoiceDate()); } ?></td>
-                        <td><?php if(isset($invoice)){echo $invoice->getAmountNet(); } ?></td>
-                        <td><?php if(isset($invoice)){echo $invoice->getAmountGross() ; }?></td>
-                        <td><?php if(isset($invoice)){echo $invoice->getAmountTax(); } ?></td>
-                        <td><?php if(isset($invoice)){echo $invoice->getCurrency() ; }?></td>
-                        <td><?php if(isset($invoice)){echo $invoice->getAmountNetCurrency() ; }?></td>
-                        <td><?php if(isset($invoice)){echo $invoice->getContractorId() ; }?></td>
-                        <td><?php if(isset($invoice)){echo '<a href="/invoices/details?id=' . $invoice->getId() . '" class="btn btn-primary">Szczegóły</a>'; ; }?></td>
-                        <td><?php if(isset($invoice)){echo '<a href="/invoices/edit?id=' . $invoice->getId() . '" class="btn btn-primary">Edytuj</a>';; } ?></td>
-                        <td><?php if(isset($invoice)){echo '<a href="/invoices/delete?id=' . $invoice->getId() . '">Usuń</a>' ; }?></td>
-                    </tr>
-                <?php
             } ?>
             </tbody>
         </table>

b77789dce2db9b35e3f370db79169d7c93ca306b
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2018-05-06 18:56:15 +0200

    Permissions constants for equipment

---
+++ controller/Equipment.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace controller;
+
+
+use core\Controller;
+
+class Equipment extends Controller
+{
+    private const RESOURCE_EQUIPMENT = "equipment";
+    private const RESOURCE_EQUIPMENT_FILE = "equipment-file";
+}

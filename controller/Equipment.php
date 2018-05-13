6cf32a46f5dfd418d600eb78f6b8c066d51e893e
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2018-05-13 14:38:59 +0200

    Add mapping for equipment model

--- controller/Equipment.php
+++ controller/Equipment.php
@@ -5,8 +5,71 @@ namespace controller;
 
 
 use core\Controller;
+use model\EquipmentDB;
+use model\EquipmentView;
+use model\Invoice;
+use model\User;
+use repository\EquipmentRepository;
+use repository\InvoicesRepository;
+use repository\UserRepository;
+use util\DateUtils;
 
 class Equipment extends Controller
 {
     private const RESOURCE_EQUIPMENT = "equipment";
+
+    /**
+     * @var EquipmentRepository
+     */
+    private $equipmentRepository;
+
+    /**
+     * @var InvoicesRepository
+     */
+    private $invoiceRepository;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(array $route_params)
+    {
+        parent::__construct($route_params);
+        $this->equipmentRepository = new EquipmentRepository();
+        $this->invoiceRepository = new InvoicesRepository();
+        $this->userRepository = new UserRepository();
+    }
+
+    private function mapToView(EquipmentDB $equipment): EquipmentView
+    {
+        if ($equipment->getInvoiceId() !== null) {
+            /** @var Invoice $invoice */
+            $invoice = $this->invoiceRepository->findById($equipment->getInvoiceId());
+        }
+
+        /** @var User $user */
+        $user = $this->userRepository->findById($equipment->getUserId());
+
+        $equipmentView = new EquipmentView();
+        $equipmentView->setId($equipment->getId())
+            ->setValidationDate($equipment->getValidationDate()->format(DateUtils::$PATTERN_DASHED_DATE))
+            ->setUserName($user->getFirstName() . ' ' . $user->getLastName())
+            ->setUserId($equipment->getUserId())
+            ->setPurchaseDate($equipment->getPurchaseDate())
+            ->setPriceNet($equipment->getPriceNet())
+            ->setNotes($equipment->getNotes())
+            ->setInventoryNumber($equipment->getInventoryNumber())
+            ->setName($equipment->getName())
+            ->setDateCreated($equipment->getDateCreated()->format(DateUtils::$PATTERN_DASHED_DATE))
+            ->setLastUpdated($equipment->getLastUpdated()->format(DateUtils::$PATTERN_DASHED_DATE))
+            ->setSerialNumber($equipment->getSerialNumber())
+            ->setInvoiceId($equipment->getInvoiceId());
+
+        if (isset($invoice)) {
+            $equipmentView->setInventoryNumber($invoice->getNumber());
+        }
+
+        return $equipmentView;
+    }
 }
